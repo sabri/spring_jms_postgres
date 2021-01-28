3ce9a298b41199ddec89f69b3f387b66d19652dd
sabri baazaoui <baazaouisabri@gmail.com>
2021-01-28 10:28:30 +0100

    Configure Security

--- src/main/java/com/sabrouch/springjmspostgres/controller/Registration.java
+++ src/main/java/com/sabrouch/springjmspostgres/controller/Registration.java
@@ -3,6 +3,7 @@ package com.sabrouch.springjmspostgres.controller;
 import com.sabrouch.springjmspostgres.service.RegistrationReguest;
 import com.sabrouch.springjmspostgres.service.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/app/v1/registration")
 public class Registration {
+
+
+
     private final RegistrationService registrationService;
+    @PostMapping
     public String register (@RequestBody RegistrationReguest request){
         return registrationService.register(request);
     }
