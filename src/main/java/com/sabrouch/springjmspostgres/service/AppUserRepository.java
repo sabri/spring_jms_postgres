acc40a08adc46b0c71684ff0f304ab6f31a94026
sabri baazaoui <baazaouisabri@gmail.com>
2021-01-28 10:17:54 +0100

    Configure Security

--- src/main/java/com/sabrouch/springjmspostgres/service/AppUserRepository.java
+++ src/main/java/com/sabrouch/springjmspostgres/service/AppUserRepository.java
@@ -1,6 +1,8 @@
 package com.sabrouch.springjmspostgres.service;
 
 import com.sabrouch.springjmspostgres.appUser.Appuser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +14,6 @@ import java.util.Optional;
  */
 @Transactional(readOnly = true)
 @Repository
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<Appuser, Long> {
     Optional<Appuser> findByEmail(String email);
 }
--- src/main/java/com/sabrouch/springjmspostgres/service/RegistrationService.java
+++ src/main/java/com/sabrouch/springjmspostgres/service/RegistrationService.java
@@ -1,10 +1,12 @@
 package com.sabrouch.springjmspostgres.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by sabrouch.
  * Date: 1/28/2021
  */
-
+@Service
 public class RegistrationService {
     public String register(RegistrationReguest request) {
         return "works";
